486af4abe9f2d5379a16f92d21e0c38a566b1838
Rodrigo Soares <soaresjw27@gmail.com>
2025-01-25 16:09:57 -0300

    Funcao sotearAmigo Funcionando

--- app.js
+++ app.js
@@ -5,27 +5,46 @@ let entrada;
 
 // Botão 'Adicionar' ativado, recebe o nome do campo e salva no array.
 function adicionarAmigo(){
+    // Caixa de Alerta se o campo estiver vazio
     if(entrada = document.querySelector('input').value == ""){
-     alert("Informe o nome de um Amigo");
-
-    }
+      alert("Por favor insira um nome.");
+       }
+   
+    // Caso o campo não esteja vazio.
+    
     else{
-         entrada = document.querySelector('input').value;
+        entrada = document.querySelector('input').value;
+
+        // Verifica se o nome já está na lista.
+        if(amigos.includes(entrada)){
+            alert("Esse nome já conta na Lista, informe outro nome.");
+            document.querySelector('input').value = '';
+            return;
+        }
         amigos.push(entrada);
         console.log({amigos}); 
-    
+
+        // Exibir o nome na tela.
         let mostrarNome = document.querySelector('h3');
-        mostrarNome.innerHTML = "Lista de Amigos: " + amigos.join( '/' );
+        mostrarNome.innerHTML = "Lista de Amigos:"+ "<br>" + amigos.join( '<br>' );
+        
+        // Limpa o campo nom do amigo.
         document.querySelector('input').value = '';
-    
-
-
     }
-   
 };  
 
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vazia.");
+        return;
+    }
 
-function mostrarNome(){
-    
-}
+    let sorteioNome = Math.floor(Math.random() * amigos.length);
+
+    let NomeEscolhido = amigos[sorteioNome];
+
+    let mostrarNome = document.querySelector('h4');
+        mostrarNome.innerHTML = " Seu Amigo secreto é: " + NomeEscolhido;
+
+};
 
