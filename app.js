6bb722319440a3876471008e9e45127fead5ee4a
Rodrigo Soares <soaresjw27@gmail.com>
2025-01-25 13:55:55 -0300

    criado a Funcao do botao Adicionar

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Variavel para salvar o nomes dos amigos.
+let amigos = [];
+
+
+function adicionarAmigo(){
+    let entrada = document.querySelector('input').value;
+        amigos.push(entrada);
+        console.log({amigos}); 
+
+   
+};  
+
+
+
+function name(params) {
+    
+}
