8dfdc6177fadc1aaf5f40490e12f4c1514a752ae
Rodrigo Soares <soaresjw27@gmail.com>
2025-01-25 14:58:01 -0300

    Caixa de Alerta e Exibicao dos Nomes na tela

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // Variavel para salvar o nomes dos amigos.
 let amigos = [];
+let entrada;
 
-
+// Botão 'Adicionar' ativado, recebe o nome do campo e salva no array.
 function adicionarAmigo(){
-    let entrada = document.querySelector('input').value;
+    if(entrada = document.querySelector('input').value == ""){
+     alert("Informe o nome de um Amigo");
+
+    }
+    else{
+         entrada = document.querySelector('input').value;
         amigos.push(entrada);
         console.log({amigos}); 
+    
+        let mostrarNome = document.querySelector('h3');
+        mostrarNome.innerHTML = "Lista de Amigos: " + amigos.join( '/' );
+        document.querySelector('input').value = '';
+    
 
+
+    }
    
 };  
 
 
-
-function name(params) {
+function mostrarNome(){
     
 }
+
